3dd50634aa0c62b2cb58b0200cf48087514e8267
Eero Vahteri <eero.vahteri@helsinki.fi>
2024-01-24 15:14:25 +0200

    response data validation, tests

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 import json
+from typing import Any
 from fastapi import FastAPI
 from models.delivery import Delivery
 from services.fee_calculator import FeeCalculator
 from models.configuration import Configuration
+from models.fee import Fee
 
 app = FastAPI()
-conf_filename = "config.json" # Adjust this if you want to use different configuration file.
+conf_filename = "config.json" # Adjust this if you want to use a different configuration file.
 
-def config_parser(configuration: Configuration):
-    """Data validation for te configuration file
+def config_validator(configuration: Configuration):
+    """Data validation for the configuration file
     """
     return configuration
 
-@app.post("/")
-def index(delivery: Delivery):
+@app.post("/", response_model=Fee)
+def index(delivery: Delivery) -> Fee:
     """Endpoint to get the delivery fee
 
     Returns:
@@ -21,7 +23,8 @@ def index(delivery: Delivery):
     """
     with open(f"../{conf_filename}", "r") as file:
         data = json.load(file)
-    configuration = config_parser(data)
+    configuration = config_validator(data)
 
     delivery_fee = FeeCalculator(configuration=configuration).calculate_fee(response_object=delivery)
-    return {"delivery_fee": delivery_fee}
+    
+    return Fee(delivery_fee=delivery_fee)
